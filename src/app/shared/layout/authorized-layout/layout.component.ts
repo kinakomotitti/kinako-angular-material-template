deb8631cef2cc29ce9a599989a4d7bae29f0c521
きなこもち <sugane0210@gmail.com>
2023-08-13 07:50:59 +0900

    modified layout

--- src/app/shared/layout/authorized-layout/layout.component.ts
+++ src/app/shared/layout/authorized-layout/layout.component.ts
@@ -1,16 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss', '../../../../styles.scss']
+  styleUrls: ['./layout.component.scss', '../../../../styles.scss'],
 })
-export class LayoutComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-
-  }
-
-}
+export class LayoutComponent {}
