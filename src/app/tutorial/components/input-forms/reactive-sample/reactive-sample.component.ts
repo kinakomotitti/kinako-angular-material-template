ffae5b5c660e2b0f64d9816352ee8806a95c31ba
きなこもち <sugane0210@gmail.com>
2023-09-02 14:56:34 +0900

    update sample

--- src/app/tutorial/components/input-forms/reactive-sample/reactive-sample.component.ts
+++ src/app/tutorial/components/input-forms/reactive-sample/reactive-sample.component.ts
@@ -1,6 +1,6 @@
 import { HeroService } from 'src/app/tutorial/services/hero.service';
 
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormControlStatus, Validators } from '@angular/forms';
 
 @Component({
@@ -11,32 +11,21 @@ import { FormControl, FormControlStatus, Validators } from '@angular/forms';
 export class ReactiveSampleComponent implements OnInit {
   formattedMessage: string[] = [];
 
-  field1 = new FormControl<string>('', {
+  @Input() field1 = new FormControl<string>('', {
     nonNullable: true,
     validators: [Validators.required],
   });
+  @Output()
+  field1Change: EventEmitter<FormControl<string>> = new EventEmitter();
 
   constructor(private _heroService: HeroService) {}
 
   ngOnInit(): void {
-    this.field1.valueChanges.subscribe((val: string) => {
-      if (val !== undefined) {
-        this.formattedMessage.push(`Hello, field1 is ${val}`);
-        this._heroService.getHeroes().subscribe((result) => {
-          console.info(result.filter((i) => i.name.includes(val)));
-        });
-      }
-    });
-    this.field1.statusChanges.subscribe((val: FormControlStatus) =>
-      this.statusChanged(val)
-    );
+    this.formattedMessage.push(`on-init`);
   }
 
   statusChanged(val: FormControlStatus) {
     this.formattedMessage.push(`Status: ${val}`);
-  }
-
-  editTesxt() {
-    this.field1.setValue(Date());
+    this.field1Change.emit(this.field1);
   }
 }
--- src/app/tutorial/screens/input-forms-sample/input-forms-sample.component.ts
+++ src/app/tutorial/screens/input-forms-sample/input-forms-sample.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input-forms-sample',
   templateUrl: './input-forms-sample.component.html',
   styleUrls: ['./input-forms-sample.component.scss'],
 })
-export class InputFormsSampleComponent {}
+export class InputFormsSampleComponent {
+  playgroundField = new FormControl<string>('', { nonNullable: true });
+}
